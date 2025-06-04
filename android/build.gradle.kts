d3d9b2644fd8fd2651ad9b1bbd075ea40b8b6732
jeffgualter <seu-email-do-github>
2025-06-04 01:23:10 -0300

    Remove duplicação de bloco android no build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,43 +1,5 @@
-android {
-    namespace = "com.example.shopgp3_flutter_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // ✅ Força o uso da versão exigida pelo WebView
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-        applicationId = "com.example.shopgp3_flutter_app"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    signingConfigs {
-        create("release") {
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            signingConfig = signingConfigs.getByName("release")
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+// build.gradle.kts (NÃO DUPLICA o bloco android aqui)
+plugins {
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
