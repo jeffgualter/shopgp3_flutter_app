6e8d2f879413cda1cde51becaea932b71ab06563
jeffgualter <seu-email-do-github>
2025-06-04 01:39:03 -0300

    Corrige conflito de versão do plugin Kotlin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,4 @@
-// build.gradle.kts (NÃO DUPLICA o bloco android aqui)
 plugins {
     id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
