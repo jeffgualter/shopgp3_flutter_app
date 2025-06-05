9b1bb33739363ec89c0cc8687d719c3e3b60ebf9
jeffgualter <seu-email-do-github>
2025-06-04 23:46:51 -0300

    Corrige caminho duplicado do keystore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
             val storePassword = keystoreProperties["storePassword"]?.toString() ?: ""
             val keyAlias = keystoreProperties["keyAlias"]?.toString() ?: ""
             val keyPassword = keystoreProperties["keyPassword"]?.toString() ?: ""
-            storeFile = file("android/app/shopgp3-release-key.jks") // ✅ Corrigido aqui
+            storeFile = file("shopgp3-release-key.jks")
             this.storePassword = storePassword
             this.keyAlias = keyAlias
             this.keyPassword = keyPassword
